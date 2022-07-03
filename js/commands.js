211ff191aeefae6622b6becadb551ded07b14f7f
LuccaRebelloToledo <luccarebtoledo@gmail.com>
2022-07-03 16:06:19 -0300

    Terminal portfolio

---
+++ js/commands.js
@@ -0,0 +1,68 @@
+var password = "luccatoledo";
+var linkedin = "https://www.linkedin.com/in/lucca-toledo/";
+var instagram = "https://www.instagram.com/luccareb/";
+var github = "https://github.com/luccarebellotoledo/";
+var email = 'mailto:luccarebtoledo@gmail.com';
+
+whois = [
+  "<br>",
+  "Olá, meu nome é Lucca",
+  "Tenho 19 anos e nasci e resido na cidade de Pouso Alegre",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "O paradoxo de “Quem é você?”: nunca sabemos, mas constantemente descobrimos.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  '<span class="command">linkedin</span>       <a href="' + linkedin + '" target="_blank">linkedin/lucca-toledo' + "</a>",
+  '<span class="command">instagram</span>      <a href="' + instagram + '" target="_blank">instagram/luccareb' + '</a>',
+  '<span class="command">github</span>         <a href="' + github + '" target="_blank">github/luccarebellotoledo' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Você agora é um adminstrador...',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "App Calculadora JS - https://github.com/LuccaRebelloToledo/App-Calculadora-JS",
+  "App DespesasPessoais - https://github.com/LuccaRebelloToledo/App-Despesas-Pessoais",
+  "Apenas 2 por minha parte, outros projetos são confidenciais.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Quem é o Lucca?',
+  '<span class="command">whoami</span>         Quem é você?',
+  '<span class="command">social</span>         Redes Sociais',
+  '<span class="command">secret</span>         Super secreto',
+  '<span class="command">projects</span>       Projetos',
+  '<span class="command">history</span>        Exibir histórico de comandos',
+  '<span class="command">help</span>           Você obviamente já sabe o que isso faz!',
+  '<span class="command">email</span>          Entre em contato por e-mail!',
+  '<span class="command">clear</span>          Limpar terminal',
+  '<span class="command">banner</span>         Exibir o cabeçalho',
+  "<br>",
+];
+
+banner = [
+  '<span class="color2">Seja bem vindo ao meu portfólio/terminal interativo!</span>',
+  "<span class=\"color2\">Para obter uma lista de comandos disponíveis, digite</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
